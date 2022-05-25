cae28066439023a525615a76567a9e8899d952bc
ariful4082 <ariful4082@gmail.com>
2022-05-25 19:54:10 +0600

    google extention with react

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import "./App.css";
 import { useApp } from "./useApp";
 
 function App() {
-  const{count, handleIncrement, handleDecrement}=useApp()
+  const { count, handleIncrement, handleDecrement } = useApp();
 
   return (
     <div className="App">
-     <div className="showCount">
-     <h1>Count: {count}</h1>
-     </div>
-     <div className="showBnt">
-     <button onClick={handleDecrement}>-</button>
-      <button onClick={handleIncrement}>+</button>
-     </div>
+      <div className="showCount">
+        <h1>Count: {count}</h1>
+      </div>
+      <div className="showBnt">
+        <button onClick={handleDecrement}>-</button>
+        <button onClick={handleIncrement}>+</button>
+      </div>
     </div>
   );
 }
